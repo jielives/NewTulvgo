96040b9c16e471b2611062bdea2e521c983c0126
jielives <1149096715@qq.com>
2017-09-12 18:11:47 +0800

    new class2

---
+++ app/src/main/java/com/yg/tulvgo/ui/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.yg.tulvgo.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yg.tulvgo.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
